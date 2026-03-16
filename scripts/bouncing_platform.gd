50a4ecd95d68652e3314dfd36ce9cfd492541251
Anne Truong <anne.truongxx@gmail.com>
2026-03-16 15:11:42 +1100

    fixed animation and particles bug

--- scripts/bouncing_platform.gd
+++ scripts/bouncing_platform.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
-@export var animationPlayer : AnimationPlayer
 @export var bounceIntensity = -300.0
+var animationPlayer : AnimationPlayer
+
+func _ready() -> void:
+	animationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	if "Player" in body.name:
-		animationPlayer.play("bounce")
+		animationPlayer.play("jump")
 		body.velocity.y += bounceIntensity
 		body.jumpNum = 0
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,6 @@ class_name Player
 @export var jump_magnitude = 2
 @export var originalPosition : Vector2
 var currentPosition: Vector2
-var jet_particles : CPUParticles2D
 var jumpLimit = 3
 var jumpNum = 0
 
@@ -14,6 +13,8 @@ var maxVelocity = 600.0
 var minVelocity = -600.0
 
 var gradient : Gradient
+
+var jetIncrement = 2
 
 @onready var audio_jump = $Animator/audio_jump
 
@@ -23,36 +24,20 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity and default movement
 	if !is_on_floor():
 		velocity += get_gravity() * delta
-	else:
+	elif is_on_floor():
 		velocity.y = jump_power
 		jumpNum = 0
-		jet_particles.emitting = false
 
 	# Add jump magnitude to velocity
 	if Input.is_action_just_pressed("jump") and !is_on_floor() and jumpNum <= jumpLimit:
 		# only if player is rising
 		if velocity.y < 0:
-			jet_particles.emitting = true
 			audio_jump.play()
 			jumpNum += 1
 			velocity.y *= jump_magnitude
-			if jumpNum == 1:
-				jet_particles.color = Color.BROWN
-				jet_particles.amount += 2
-			elif jumpNum == 2:
-				jet_particles.color = Color.ORANGE_RED
-				jet_particles.amount += 2
-			elif jumpNum == 3:
-				jet_particles.color = Color.RED
-				jet_particles.amount += 2
-			elif jumpNum == 4 and Input.is_action_pressed("jump"):
-					velocity.y = -10
-					jet_particles.color = Color.BLACK
-					jet_particles.amount += 2
-			else:
-				jet_particles.color = Color.DARK_GRAY
-				jet_particles.amount = 3
-		
+			if jumpNum == 4:
+				velocity.y = 0
+
 	## Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
 	if direction:
